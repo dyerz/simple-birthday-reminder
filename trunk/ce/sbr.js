e7898b61dd65bd9804d20e6a417cf51b31e8cb62
adamdyer@gmail.com <adamdyer@gmail.com@163606a5-0136-739a-be38-741aaec2f622>
2013-12-02 08:20:34 +0000

    version 1.2.1 - more bugfixes and added a screenshot

--- trunk/ce/sbr.js
+++ trunk/ce/sbr.js
@@ -7,9 +7,9 @@ SimpleBirthdayReminder = function(){
 	self = this;
 
 	// test client id
-//	self.clientId = '310837256871-nv2otqr7qojvir1u6enjqh2s7h71tvcj.apps.googleusercontent.com';
+	self.clientId = '310837256871-nv2otqr7qojvir1u6enjqh2s7h71tvcj.apps.googleusercontent.com';
 	// store client id
-	self.clientId = '310837256871-q21g2ngu6vjke564upgul2lf6ssvqg56.apps.googleusercontent.com';
+//	self.clientId = '310837256871-q21g2ngu6vjke564upgul2lf6ssvqg56.apps.googleusercontent.com';
 
 	self.scopes = 'https://www.googleapis.com/auth/drive https://spreadsheets.google.com/feeds';
 	
